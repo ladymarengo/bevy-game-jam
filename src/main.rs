6658a033b62fd15979effc0fe96380fed4d73ea8
Filipp Samoilov <samoylovfp@gmail.com>
2022-02-26 19:32:58 +0200

    Add background
    glory to Ukraine

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_system(bevy::input::system::exit_on_esc_system)
+        .add_startup_system(background)
         .add_startup_system(init)
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_ground)
@@ -29,6 +30,7 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
             custom_size: Some(Vec2::new(75.0, 50.0)),
             ..Default::default()
         },
+        transform: Transform::from_translation(Vec3::new(0.0, -350.0, 2.0)),
         ..Default::default()
     });
 }
@@ -36,11 +38,21 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn spawn_ground(mut commands: Commands) {
     commands.spawn_bundle(SpriteBundle {
         sprite: Sprite {
-            custom_size: Some(Vec2::new(2000.0, 50.0)),
+            custom_size: Some(Vec2::new(2000.0, 100.0)),
             color: Color::rgb(0.76, 0.55, 0.10),
             ..Default::default()
         },
-        transform: Transform::from_translation(Vec3::new(0.0, -50.0, 0.0)),
+        transform: Transform::from_translation(Vec3::new(0.0, -400.0, 1.0)),
+        ..Default::default()
+    });
+}
+
+
+fn background(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // FIXME: Needs to be synched with the camera
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("bg.png"),
+        // transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
         ..Default::default()
     });
 }
