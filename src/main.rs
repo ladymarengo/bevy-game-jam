ca45664a9b5411940c63920683e667ad54c894e0
kolen <incredible.angst@gmail.com>
2022-03-02 04:36:17 +0300

    Add main character sprite

--- src/main.rs
+++ src/main.rs
@@ -34,32 +34,53 @@ fn set_window_resolution(mut windows: ResMut<Windows>) {
         .set_resolution(256.0 * PIXEL_MULTIPLIER, 215.0 * PIXEL_MULTIPLIER);
 }
 
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("f.png"),
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(75.0, 50.0)),
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture = asset_server.load("main_character.png");
+    let texture_atlas = TextureAtlas::from_grid(texture, Vec2::new(32.0, 16.0), 4, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle.clone(),
+            sprite: TextureAtlasSprite {
+                index: 0,
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 2.0)),
             ..Default::default()
-        },
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 2.0)),
-        ..Default::default()
-    }).insert(Player);
+        })
+        .insert(Player);
 }
 
-fn player_move(mut player: Query<&mut Transform, With<Player>>, keys: Res<Input<KeyCode>>) {
-    let mut player_transform = player.single_mut();
+fn player_move(
+    mut player: Query<(&mut Transform, &mut TextureAtlasSprite), With<Player>>,
+    keys: Res<Input<KeyCode>>,
+) {
+    let (mut player_transform, mut texture_atlas_sprite) = player.single_mut();
+    let mut moved = false;
 
     if keys.pressed(KeyCode::W) {
         player_transform.translation.y += 1.0;
+        moved = true;
     }
     if keys.pressed(KeyCode::S) {
         player_transform.translation.y -= 1.0;
+        moved = true;
     }
     if keys.pressed(KeyCode::A) {
         player_transform.translation.x -= 1.0;
+        moved = true;
     }
     if keys.pressed(KeyCode::D) {
         player_transform.translation.x += 1.0;
+        moved = true;
     }
 
+    // TODO: add timer instead of changing every frame
+    if moved {
+        texture_atlas_sprite.index = (texture_atlas_sprite.index + 1) % 4;
+    }
 }
