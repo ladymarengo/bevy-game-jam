743b9e12128c8f260447c3d1832d1b1ca6b1f8f9
Filipp Samoilov <samoylovfp@gmail.com>
2022-03-04 17:13:01 +0200

    Add hp counter

---
+++ src/hud.rs
@@ -0,0 +1,46 @@
+use bevy::prelude::*;
+
+use crate::Hp;
+
+
+#[derive(Component)]
+pub struct HpLabel;
+
+pub fn spawn_hp_meter(mut commands: Commands, asset_server: Res<AssetServer>) {
+
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    // root node
+    commands.spawn_bundle(NodeBundle {
+        style: Style {
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            justify_content: JustifyContent::SpaceBetween,
+            align_items: AlignItems::FlexEnd,
+            ..Default::default()
+        },
+        color: Color::NONE.into(),
+        ..Default::default()
+    }).with_children(|parent| {
+        parent.spawn_bundle(TextBundle {
+            style: Style {
+                margin: Rect::all(Val::Px(5.0)),
+                ..Default::default()
+            },
+            text: Text::with_section(
+                "hello!",
+                TextStyle {
+                    font: asset_server.load("PublicPixel-0W6DP.ttf"),
+                    font_size: 30.0,
+                    color: Color::WHITE,
+                },
+                Default::default(),
+            ),
+            ..Default::default()
+        }).insert(HpLabel);
+    });
+
+}
+
+pub fn update_hp_meter(mut hp_label: Query<&mut Text, With<HpLabel>>, hp: Res<Hp>) {
+    hp_label.single_mut().sections[0].value = format!("hp {}", hp.0);
+}
